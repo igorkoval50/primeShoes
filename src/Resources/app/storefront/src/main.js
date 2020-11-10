4e51241a8a3e7f507dfc9a475d7e8110e5de98e4
user <igor.k@ioksa.company>
2020-11-11 00:43:39 +0200

    change style for the look page

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-    let heightSlider = $('.home-slider-first').height();
-    console.log(heightSlider);
-    console.log($('window').offset().top);
-        if($('window').offset().top > heightSlider) {
-            $('.header-main').addClass('header-black');
-            console.log(1);
-        } else {
-            $('.header-main').removeClass('header-black');
-            console.log(2);
-        }
+    let heightSlider = $('.home-slider-first').height() - 20;
+    if($('body').hasClass('is-act-home')) {
+        $(window).on('scroll', function() {
+            var fromTop = $(window).scrollTop();
+            if (fromTop > heightSlider) {
+                $('.header-main').addClass('header-black');
+            } else {
+                $('.header-main').removeClass('header-black');
+            }
+        });
+    }
 });
