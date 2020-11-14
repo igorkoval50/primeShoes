ffad854844202d007c0a6564bb77d3db31b35e84
user <igor.k@ioksa.company>
2020-11-14 22:49:06 +0200

    change slider view

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
     //stick header in home page if height of the slider
-    if($('body').hasClass('is-act-home')) {
-        var $win = $(window);
-        var winH = $win.height() - 20;   // Get the window height.
+    let $win = $(window);
+    let winH = $win.height() - 20;   // Get the window height.
 
+    $win.on("resize", function() {
+        winH = $(this).height();
+    });
+
+    if($('body').hasClass('is-act-home')) {
         $win.on("scroll", function () {
             if ($(this).scrollTop() > winH ) {
                 $('.header-main').addClass('header-black');
             } else {
                 $('.header-main').removeClass('header-black');
             }
-        }).on("resize", function(){
-            winH = $(this).height();
         });
     }
 
@@ -22,7 +24,6 @@ $(document).ready(function () {
 
     //in look page click at the botton move down
     $('.move-down').on('click', function () {
-        let heightDown = $('.look-banner').height();
-        window.scrollTo(0, heightDown);
+        window.scrollTo(0, winH);
     });
 });
