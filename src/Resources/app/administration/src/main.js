bd20a3fdd1b4294a08a4ac7761c8853b55bbe2f3
Igor <kigor2152@gmail.com>
2020-12-11 14:33:27 +0200

    add banner element

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,1 +1,3 @@
+import './module/sw-cms/blocks/text-image/image-text-row-one';
 import './module/sw-cms/blocks/text-image/image-text-row-two';
+
--- src/Resources/app/administration/src/module/sw-cms/blocks/text-image/image-text-row-one/index.js
+++ src/Resources/app/administration/src/module/sw-cms/blocks/text-image/image-text-row-one/index.js
@@ -2,17 +2,17 @@ import './component';
 import './preview';
 
 Shopware.Service('cmsService').registerCmsBlock({
-    name: 'image-text-row-two',
-    label: 'sw-cms.blocks.imageText.imageTextInside.label',
+    name: 'image-text-row-one',
+    label: 'sw-cms.blocks.imageText.bannerTextInside.label',
     category: 'text-image',
-    component: 'sw-cms-block-image-text-row-two',
-    previewComponent: 'sw-cms-preview-image-text-row-two',
+    component: 'sw-cms-block-image-text-row-one',
+    previewComponent: 'sw-cms-preview-image-text-row-one',
     defaultConfig: {
         marginBottom: '20px',
         marginTop: '20px',
         marginLeft: '0px',
         marginRight: '0px',
-        sizingMode: 'boxed'
+        sizingMode: 'full_width'
     },
     slots: {
         'left-image': {
@@ -29,33 +29,6 @@ Shopware.Service('cmsService').registerCmsBlock({
             }
         },
         'left-text': {
-            type: 'text',
-            default: {
-                config: {
-                    content: {
-                        source: 'static',
-                        value: `
-                        <p>Headline Information</p>
-                        <a href="#" class="btn btn-primary">Show more</a>
-                        `.trim()
-                    }
-                }
-            }
-        },
-        'center-image': {
-            type: 'image',
-            default: {
-                config: {
-                    displayMode: { source: 'static', value: 'cover' }
-                },
-                data: {
-                    media: {
-                        url: '/administration/static/img/cms/preview_plant_large.jpg'
-                    }
-                }
-            }
-        },
-        'center-text': {
             type: 'text',
             default: {
                 config: {
